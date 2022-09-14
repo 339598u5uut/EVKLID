6ad9fac4853a9f892e1116168e939f5d43fa6411
Kiva Natalia <violett@mail.ru>
2022-09-14 13:33:01 +0300

    add update

--- js/index.js
+++ js/index.js
@@ -1,34 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    document.querySelectorAll('.section-work__list-btn').forEach(function(sectionWorkListBtn) {
-        sectionWorkListBtn.addEventListener('click', function(event) {
-            const path = event.currentTarget.dataset.path
-
-            document.querySelectorAll('.section-work__content').forEach(function(sectionWorkContent) {
-                sectionWorkContent.classList.remove('section-work__content-active')
-            })
-            document.querySelector(`[data-target="${path}"]`).classList.add('section-work__content-active')
-        })
-    })
-});
-
-
-const swiper = new Swiper('.swiper-container', {
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-
-    scrollbar: {
-        hide: 'true',
-    },
-});
-
-window.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#header__burger').addEventListener('click', function() {
-        document.querySelector('#menu').classList.toggle('is-active')
-    })
-})
+document.addEventListener('DOMContentLoaded',function(){document.querySelectorAll('.section-work__list-btn').forEach(function(sectionWorkListBtn){sectionWorkListBtn.addEventListener('click',function(event){const path=event.currentTarget.dataset.path
+  document.querySelectorAll('.section-work__content').forEach(function(sectionWorkContent){sectionWorkContent.classList.remove('section-work__content-active')})
+  document.querySelector(`[data-target="${path}"]`).classList.add('section-work__content-active')})})});const swiper=new Swiper('.swiper-container',{loop:!0,pagination:{el:'.swiper-pagination',clickable:!0,},scrollbar:{hide:'true',},});window.addEventListener('DOMContentLoaded',function(){document.querySelector('#header__burger').addEventListener('click',function(){document.querySelector('#menu').classList.toggle('is-active')})})
